191bd65b738a31c318e856539b23ddf0437f9455
unknown <hertzrafael@hotmail.com>
2024-09-21 13:44:05 -0300

    extract method and data files added.

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
+from extract import extract_all
+from actions import Actions
 
 def run():
-    print('Aplicação iniciada!')
-    pass
+    frame = extract_all()
+
+    actions = Actions(frame)
+    student_notes = actions.get_notes_by_student(1)
+
+    print(actions)
 
 if __name__ == '__main__':
     run()
